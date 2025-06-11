b6e4a98979edc8afd75013bb396de4bb71491eda
Tre Rush <tre.rush@toshibagcs.com>
2025-06-11 00:39:37 -0400

    Removed time only datepicker and refactored some of the styling.

--- src/components/CustomDateInput.tsx
+++ src/components/CustomDateInput.tsx
@@ -27,21 +27,21 @@ export default function CustomDateInput(
         <div
             onClick={onClick}
             ref={ref }
-            className="custom-date-time-picker"
+            className="custom-input-container"
         >
-            <span className={"custom-date-time-picker-icon"}>{icon}</span>
-            <span className={"custom-date-time-picker-value"}>{value || text}</span>
+            <span className={"custom-input-icon"}>{icon}</span>
+            <span className={"custom-input-value"}>{value || text}</span>
         </div>
     ));
 
 
 
     return (
-        <div className={"custom-date-time-picker-container"}>
-            <div className={"custom-date-time-pick"}>
+        <div className={"custom-react-datetime-picker-container"}>
+            <div className={"custom-react-datetime-picker"}>
                 <DatePicker
                     selected={startDate}
-                    dateFormat="MM/dd/yyyy"
+                    dateFormat="MM/dd/yyyy h:mm aa"
                     customInput={<CustomInput icon={<CiCalendar size={20}/>} placeholder={"Start Date"} text={"Start Date"}/>}
                     locale={enUS}
                     onChange={onStartDateChange}
@@ -49,51 +49,28 @@ export default function CustomDateInput(
                     showYearDropdown
                     dropdownMode="select"
                     yearDropdownItemNumber={10}
-                />
-                {/*<div className={"custom-time-input-container"}>*/}
-                {/*    <span>{<CiClock2 size={20}/>}</span>*/}
-                {/*    <input className={"custom-time-input"}*/}
-                {/*           type={"time"}*/}
-                {/*    />*/}
-                {/*</div>*/}
-                <span className={"spacer"}></span>
-                <DatePicker
-                    selected={startTime}
                     showTimeSelect
-                    showTimeSelectOnly
+                    timeFormat="h:mm aa"
                     timeIntervals={15}
-                    timeCaption="Time"
-                    dateFormat="h:mm aa"
-                    customInput={<CustomInput icon={<CiClock2 size={20}/>} text={"Time"} />}
-                    locale={enUS}
-                    onChange={onStartTimeChange}
-                    />
+                    isClearable
+                />
             </div>
             <span>to</span>
-            <div className={"custom-date-time-pick"}>
+            <div className={"custom-react-datetime-picker"}>
                 <DatePicker
                     selected={endDate}
-                    dateFormat="MM/dd/yyyy"
+                    dateFormat="MM/dd/yyyy h:mm aa"
                     customInput={<CustomInput icon={<CiCalendar size={20}/>} placeholder={"End Date"} text={"End Date"} />}
                     locale={enUS}
                     onChange={onEndDateChange}
-
                     showMonthDropdown
                     showYearDropdown
                     dropdownMode="select"
                     yearDropdownItemNumber={10}
-                />
-                <span className={"spacer"}></span>
-                <DatePicker
-                    selected={endTime}
                     showTimeSelect
-                    showTimeSelectOnly
+                    timeFormat="h:mm aa"
                     timeIntervals={15}
-                    timeCaption="Time"
-                    dateFormat="h:mm aa"
-                    customInput={<CustomInput icon={<CiClock2 size={20}/>} text={"Time"} />}
-                    locale={enUS}
-                    onChange={onEndTimeChange}
+                    isClearable
                 />
             </div>
         </div>
